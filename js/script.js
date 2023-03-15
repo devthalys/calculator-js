75f9cd989563f6d57e1a4e09527a62648798775f
Thalys001 <thalyssouza708@gmail.com>
2023-03-14 23:47:53 -0300

    feat: adding digit on screen

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
-const previousOperationText = document.querySelector("#previous-operation")
-const currentOperationText = document.querySelector("#current-operation")
-const buttons = document.querySelectorAll("#buttons-container button")
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
 
 class Calculator {
+  constructor(previousOperationText, currentOperationText) {
+    this.previousOperationText = previousOperationText;
+    this.currentOperationText = currentOperationText;
+    this.currentOperation = "";
+  }
 
+
+  addDigit(digit) {
+    
+    this.currentOperation = digit
+    this.updateScreen()
+  }
+
+  updateScreen() {
+    this.currentOperationText.innerText += this.currentOperation
+  }
 }
+
+const calc = new Calculator(previousOperationText, currentOperationText);
 
 // ativando eventos dos botões
 buttons.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        const value = e.target.innerText
+  btn.addEventListener("click", (e) => {
+    const value = e.target.innerText;
 
-        if(+value >= 0 || value === ".") {
-            console.log(value);
-        } else {
-            console.log("Op: " + value);
-        }
-    })
-})
+    if (+value >= 0 || value === ".") {
+      calc.addDigit(value);
+    } else {
+      console.log("Op: " + value);
+    }
+  });
+});
